46da9d70819b0c914abdb22fee03343f3b3dca90
didiciolpan <didi.ciolpan@yahoo.com>
2021-05-14 23:18:43 +0300

    model class exponate

---
+++ Museum/Models/Exponate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Museum.Models
+{
+    public class Exponate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+    }
+}
