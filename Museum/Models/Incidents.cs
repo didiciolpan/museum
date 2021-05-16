3426ba140e1084af6d2c87babdc9dbaf0beae211
alexandra_colhon <alexandra_colhon@yahoo.com>
2021-05-16 11:20:44 +0300

    Incidents class created

---
+++ Museum/Models/Incidents.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Muzeu.Models
+{
+    public class Incidents
+    {
+        
+        public int Id { set; get; }
+
+        [Required]
+        public string Name { set; get; }
+
+
+        public string Description { set; get; }
+
+        [Required]
+        public string Priority { set; get; }
+
+        [Required]
+        public DateTime Date { set; get; }
+
+        [Required]
+        public Boolean Solved { set; get; }
+    }
+}
