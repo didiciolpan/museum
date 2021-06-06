9b3d210b81f45fddd948063b4a427f79a4224c97
andreiCraiu <64604622+andreiCraiu@users.noreply.github.com>
2021-06-06 13:33:30 +0300

    Add migrations

---
+++ Museum/Data/Migrations/20210606103256_addEntityes.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Museum.Data.Migrations
+{
+    public partial class addEntityes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Report",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Report", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SecurityGuards",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    ReportID = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SecurityGuards", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_SecurityGuards_Report_ReportID",
+                        column: x => x.ReportID,
+                        principalTable: "Report",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SecurityGuards_ReportID",
+                table: "SecurityGuards",
+                column: "ReportID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SecurityGuards");
+
+            migrationBuilder.DropTable(
+                name: "Report");
+        }
+    }
+}
